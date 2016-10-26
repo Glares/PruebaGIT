ecee2e79df9e4459d7fdda53be17986fc4563176
alu13-dam2 <alu13-dam2>
2016-10-26 08:42:43 +0200

    sumemos

---
+++ PruebaGIT.java
@@ -0,0 +1,11 @@
+
+public class PruebaGIT {
+
+	public static void main(String[] args) {
+		int a= 3;
+		int b= 5;
+		int c= a + b;
+		System.out.println("la suma de "+a+" + "+b + "es:"+c);
+	}
+
+}
